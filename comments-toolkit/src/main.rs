666b8ee5734a174c0f5e29e8af7734e1b0c552b1
georgefkd <geonfak76@gmail.com>
2025-10-15 12:53:51 +0300

    getting files and parsing arguments is now properly done

--- comments-toolkit/src/main.rs
+++ comments-toolkit/src/main.rs
@@ -1,27 +1,132 @@
 use std::collections::HashMap;
 use std::env::Args;
+use std::fs::read_dir;
 use std::iter::Iterator;
-use std::{env, u64};
+use std::path::{Path, PathBuf};
+use std::{env, panic, u64};
+
+use storage::SqliteDB;
 fn main() {
+    use parser::Parser;
+    use storage::Storage;
     let program_args = env::args();
+
+    let options = parse_program_args(program_args)
+        .inspect_err(|e| eprintln!("Error while parsing args: {e}"))
+        .unwrap();
+
+    assert!(options.len() > 0);
+
+    match are_args_valid(&options) {
+        Ok(()) => println!("Check: Arguments are valid"),
+        Err(e) => eprintln!("Error while checking option combinations validity {e}"),
+    }
+
+    let project_files = get_files_from_directory_recursively(
+        options
+            .get("source")
+            .expect("should provide --source flag")
+            .into(),
+        &vec![
+            "assets".to_owned(),
+            "build".to_owned(),
+            "cmake-build-debug".to_owned(),
+        ],
+        &vec!["h".to_owned(), "cpp".to_owned()],
+    );
+
+    println!("The project files are: {}", project_files.len());
 
     println!("Hello, world!, this is something new");
 }
 
-fn parse_program_args(args: Args) -> HashMap<String, String> {
-    //the format is: --<argname1><space><value><space>--<argname2>
-    return HashMap::new();
+fn get_files_from_directory_recursively(
+    dir: PathBuf,
+    ignored_dirs: &Vec<String>,
+    file_extensions_allowed: &Vec<String>,
+) -> Vec<PathBuf> {
+    println!("The dir is: {:?}", dir);
+    //the performance might be bad
+    assert!(dir.is_dir());
+    match read_dir(dir) {
+        Err(e) => vec![],
+        Ok(entries) => entries
+            .filter(|p| p.is_ok())
+            .flat_map(|p| {
+                let p = p.unwrap().path();
+                let last_path = p.as_path().iter().last().unwrap().to_str().unwrap();
+                match p.is_dir()
+                    && !ignored_dirs.contains(&last_path.to_owned())
+                    && !last_path.starts_with(".")
+                {
+                    true => get_files_from_directory_recursively(
+                        p,
+                        ignored_dirs,
+                        file_extensions_allowed,
+                    ),
+                    false => {
+                        let mut result = vec![];
+                        if let Some(v) = p.extension() {
+                            if file_extensions_allowed.contains(&v.to_str().unwrap().to_owned()) {
+                                result.push(p);
+                            }
+                        }
+                        return result;
+                    }
+                }
+            })
+            .collect(),
+    }
 }
 
-fn are_args_valid(args: &HashMap<String, String>) -> bool {
-    return true;
+fn parse_program_args(args: Args) -> Result<HashMap<String, String>, &'static str> {
+    //the format is: --<argname1><space><value><space>--<argname2>
+    //no need for a library
+
+    let mut args: Vec<String> = args.collect();
+    if args.len() == 1 {
+        return Err("No arguments passed to executable, can display help page here.");
+    }
+
+    let mut args = args.into_iter();
+
+    let mut result = HashMap::new();
+    args.skip(1)
+        .reduce(|acc, s| return String::from(acc) + " " + &s)
+        .expect("It was previously checked that we have enough arguments")
+        .split("--")
+        .skip(1)
+        .for_each(|arg| {
+            let mut key_val_pair = arg.split(" ");
+            let key = key_val_pair.next().unwrap();
+            let val = key_val_pair.next();
+            match val {
+                None => result.insert(key.to_owned(), "".to_owned()),
+                Some(v) => result.insert(key.to_owned(), v.to_owned()),
+            };
+        });
+
+    return Ok(result);
+}
+
+fn are_args_valid(args: &HashMap<String, String>) -> Result<(), &'static str> {
+    //this is just some business logic for validating mutually exclusive params etc. etc.
+    return Ok(());
 }
 
 mod storage {
-    use super::*;
+    use super::models;
+    use rusqlite::Connection;
     use std::path::PathBuf;
-    struct SqliteDB {
+    pub struct SqliteDB {
         file: PathBuf,
+    }
+
+    impl SqliteDB {
+        pub fn new(file: PathBuf) -> Result<Self, rusqlite::Error> {
+            let conn = Connection::open(&file)?;
+            return Ok(Self { file });
+        }
     }
 
     impl Storage for SqliteDB {
@@ -78,11 +183,14 @@ mod storage {
     }
 }
 mod parser {
+    use crate::models::CommentData;
+
     use super::{models, storage};
     use std::collections::HashMap;
-    use std::iter::Iterator;
-    use std::path::Path;
-    enum ParserPositionType {
+    use std::path::{Path, PathBuf};
+
+    //we will be ignoring the inline comments probably
+    pub enum ParserPositionType {
         // has seen the character /*
         IN_MULTILINE_COMMENT,
         // has seen the character //
@@ -95,11 +203,27 @@ mod parser {
     }
 
     //The parser assumes the options are all valid
-    struct Parser {
+    //we dont need ownership of the options
+    pub struct Parser {
         options: HashMap<String, String>,
-        state: ParserState,
+        state: Option<ParserState>,
         //if performance gets bad this might be changed to SQLite to avoid the vtable cost
         storage: Box<dyn storage::Storage>,
+    }
+
+    impl Parser {
+        pub fn new(options: HashMap<String, String>, storage: Box<dyn storage::Storage>) -> Self {
+            Parser {
+                options,
+                storage,
+                state: None,
+            }
+        }
+
+        pub fn debug_dump(&self) {
+            println!("Options are {:?}", &self.options);
+            println!("DB is sqlite");
+        }
     }
 
     fn project_folder() -> String {
@@ -116,23 +240,36 @@ mod parser {
         todo!()
     }
 
-    fn parse_project_in(directory: &Path) -> bool {
-        assert!(directory.is_dir());
-        //this should call parse_file
-        return false;
+    //this does not belong here
+    fn apply_integrity_rules(
+        parsed_comments: Vec<Vec<(PathBuf, CommentData)>>,
+    ) -> Result<(), String> {
+        return Ok(());
     }
 
-    fn parse_file(file: &Path) -> bool {
-        return false;
+    fn parse_project_in(directory: &Path) -> Vec<Vec<(PathBuf, CommentData)>> {
+        assert!(directory.is_dir());
+        //this should call parse_file
+        return Vec::new();
+    }
+
+    fn parse_file(file: &Path) -> Vec<CommentData> {
+        return Vec::new();
     }
 }
 pub mod models {
     use std::{hash::Hash, path::PathBuf};
 
+    pub enum CommentReference {
+        WholeFile(PathBuf),
+        Code(String),
+    }
+
     pub struct CommentData {
         location: SourceRange,
         //used in the generation of the hash
         raw_contents: String,
+        refers_to: CommentReference,
         file: PathBuf,
         hash: String,
     }
@@ -140,7 +277,7 @@ pub mod models {
     impl Hash for CommentData {
         fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
             todo!()
-            //using only the contents and the filename, also the content should first be split into
+            //using only the code contents and the filename, also the content should first be split into
             //words, also certain characters should be ignored.
         }
     }
